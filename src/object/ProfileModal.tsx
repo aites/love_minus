de37bd86efd90e54857f052f7bb048e2ddfcd588
Ryoga Komine <>
2020-04-18 13:23:57 +0900

    タイムライン追加

---
+++ src/object/ProfileModal.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Paper, Typography, Grid, Button, Box, Modal } from '@material-ui/core';
+import { ListProfileInterface } from './ProfileListCard';
+
+/**
+ * ユーザープロフィールのモーダル
+ * @param prof 
+ */
+function ProfileModal(prof: ListProfileInterface) {
+  return (
+    <Paper style={{width:100, height:100, }}>
+
+    </Paper>
+  );
+}
+
+export default ProfileModal;
