3730a0fbe2b85658dfaa06a53a111511f852ab52
yun8686 <yun.mes41@gmail.com>
2020-05-04 14:50:53 +0900

    mailBoxデザイン微調整

--- src/page/MailBox.tsx
+++ src/page/MailBox.tsx
@@ -27,7 +27,7 @@ export default class MailBox extends React.Component {
             );
           })}
         </Grid>
-        <Grid item xs={12} sm={8} md={9} className={classes.content}>
+        <Grid item xs={12} sm={8} md={9} className={classes.content} style={{ overflow: 'hidden' }}>
           <ChatRoom />
         </Grid>
       </Grid>
