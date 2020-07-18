a6e034d41a2b78f5fceacc256fe7ab1a89bc56c9
a.aguri <>
2020-07-18 20:35:16 +0900

    PC画面；トークルームのモーダルでアイコン選択する処理を追加

--- src/object/ProfileModal.tsx
+++ src/object/ProfileModal.tsx
@@ -8,6 +8,7 @@ import {
   InputLabel,
   Select,
   MenuItem,
+  Grid,
 } from '@material-ui/core';
 import NavigateBefore from '@material-ui/icons/NavigateBefore';
 import NavigateNext from '@material-ui/icons/NavigateNext';
@@ -15,6 +16,7 @@ import classes from '../scss/object/profileModal.module.scss';
 import { Profile } from '../modules/models/Profile';
 import { createChatRoom } from '../modules/models/Chatroom';
 import { getCurrentUser } from '../modules/firebase';
+import { characterList } from '../modules/models/Character';
 
 type ProfileModalProps = {
   profile: Profile;
@@ -25,6 +27,9 @@ type ProfileModalStates = {
   name: string;
   sex: 'man' | 'woman';
   profile: Profile;
+  icon: string;
+  minIcon: string;
+  selected: Array<Boolean>;
 };
 
 export default class ProfileModal extends Component<ProfileModalProps, ProfileModalStates> {
@@ -34,6 +39,9 @@ export default class ProfileModal extends Component<ProfileModalProps, ProfileMo
       profile: props.profile,
       sex: 'man',
       name: '',
+      icon: '',
+      minIcon: '',
+      selected: [],
     };
     this.createChatroom = this.createChatroom.bind(this);
   }
@@ -52,14 +60,21 @@ export default class ProfileModal extends Component<ProfileModalProps, ProfileMo
       playerInfo: {
         name: this.state.name,
         sex: this.state.sex,
-        icon: '',
-        miniIcon: '',
+        icon: this.state.icon,
+        miniIcon: this.state.minIcon,
         profile: '',
         simpleProf: '',
         author: playerUid,
       },
     });
   }
+
+  selectedIcon(i: number) {
+    const selected_copy = Array(characterList.length).fill(false);
+    selected_copy[i] = true;
+    this.setState({ selected: selected_copy });
+  }
+
   render() {
     const prof = this.state.profile;
     return (
@@ -73,44 +88,69 @@ export default class ProfileModal extends Component<ProfileModalProps, ProfileMo
           <p className={classes.name}>{prof.name}</p>
           <p className={classes.profile}>{prof.profile}</p>
           {this.props.loginUserUid !== prof.author ? (
-            <Box display="flex">
-              <TextField
-                id="name"
-                label="名前"
-                variant="outlined"
-                className={classes.textfield}
-                value={this.state.name}
-                onChange={(e) => {
-                  this.setState({ name: e.target.value });
-                }}
-              ></TextField>
-              <FormControl variant="outlined" className={classes.textfield}>
-                <InputLabel id="sex">性別</InputLabel>
-                <Select
-                  labelId="sex"
-                  id="sex"
-                  value={this.state.sex}
+            <>
+              <p>キャラクターアイコン</p>
+              <Grid container direction="row" justify="center" alignItems="center">
+                {characterList.map((v, i) => {
+                  return (
+                    <Grid item xs={2} className={classes.icon_content}>
+                      <Paper
+                        className={classes.image_icon_wrap}
+                        elevation={this.state.selected[i] ? 4 : 1}
+                      >
+                        <img
+                          className={classes.image_icon}
+                          src={v.icon}
+                          alt=""
+                          onClick={() => {
+                            this.setState({ icon: v.image, minIcon: v.icon });
+                            this.selectedIcon(i);
+                          }}
+                        />
+                      </Paper>
+                    </Grid>
+                  );
+                })}
+              </Grid>
+              <Box display="flex" className={classes.inputWrapper}>
+                <TextField
+                  id="name"
+                  label="名前"
+                  variant="outlined"
+                  className={classes.textfield}
+                  value={this.state.name}
                   onChange={(e) => {
-                    this.setState({ sex: e.target.value as 'man' | 'woman' });
+                    this.setState({ name: e.target.value });
                   }}
-                  label="性別"
+                ></TextField>
+                <FormControl variant="outlined" className={classes.textfield}>
+                  <InputLabel id="sex">性別</InputLabel>
+                  <Select
+                    labelId="sex"
+                    id="sex"
+                    value={this.state.sex}
+                    onChange={(e) => {
+                      this.setState({ sex: e.target.value as 'man' | 'woman' });
+                    }}
+                    label="性別"
+                  >
+                    <MenuItem value={'man'}>男</MenuItem>
+                    <MenuItem value={'woman'}>女</MenuItem>
+                  </Select>
+                </FormControl>
+                <Button
+                  style={{ marginLeft: 'auto' }}
+                  color="secondary"
+                  variant="outlined"
+                  onClick={async () => {
+                    const chatRoom = await this.createChatroom();
+                    this.props.onCreateChatroom(chatRoom);
+                  }}
                 >
-                  <MenuItem value={'man'}>男</MenuItem>
-                  <MenuItem value={'woman'}>女</MenuItem>
-                </Select>
-              </FormControl>
-              <Button
-                style={{ marginLeft: 'auto' }}
-                color="secondary"
-                variant="outlined"
-                onClick={async () => {
-                  const chatRoom = await this.createChatroom();
-                  this.props.onCreateChatroom(chatRoom);
-                }}
-              >
-                チャット開始
-              </Button>
-            </Box>
+                  チャット開始
+                </Button>
+              </Box>
+            </>
           ) : null}
         </Box>
       </Paper>
